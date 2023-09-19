ca5d4475bc401da3325cefedd15515e9bd3f2364
merv-e <34655454+merv-e@users.noreply.github.com>
2023-09-19 13:21:35 +0300

    Customized colors are  added to tailwind and now being used in the website.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './build/**/*.html'
-  ],
+  content: ["./build/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main_blue: "rgb(23, 0, 192)",
+        light_blue: "rgb(75, 176, 255)",
+        light_pink: "rgb(255, 130, 248)",
+        main_pink: "rgb(255, 0, 102)",
+        main_yellow: "rgb(255, 199, 0)",
+      },
+    },
   },
   plugins: [],
-}
+};
+
+
 
