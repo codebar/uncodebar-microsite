846c149795693f986b0a34e26fb3980a8fb96a31
merv-e <34655454+merv-e@users.noreply.github.com>
2023-09-19 22:14:34 +0300

    - the color of buttons is changed. - some minor changes in styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,19 @@ module.exports = {
         light_pink: "rgb(255, 130, 248)",
         main_pink: "rgb(255, 0, 102)",
         main_yellow: "rgb(255, 199, 0)",
+        turquoise: "#02fecd",
+        button_blue: "#3748fa",
+        button_blue_: "#6557f5",
       },
       boxShadow: {
         "3xl": "20px 0px 400px 20px #FFFFFF",
       },
       borderWidth: {
-        DEFAULT: '0.5px',
-      }
+        DEFAULT: "0.5px",
+      },
     },
     plugins: [],
-  }
-}
+  },
+};
 
 
